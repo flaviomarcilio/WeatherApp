46820a07a760c20dde6035e9d58202c0f1530bb8
flaviomarcilio <flaviomarciliodeoliveira@gmail.com>
2020-11-25 19:53:29 -0300

    Cria current-weather component

---
+++ src/app/shared/components/components.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoaderComponent } from './loader/loader.component';
+import { DetailedWeatherComponent } from './detailed-weather/detailed-weather.component';
+
+
+@NgModule({
+  declarations: [
+    LoaderComponent,
+    DetailedWeatherComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LoaderComponent,
+    DetailedWeatherComponent
+  ]
+})
+export class ComponentsModule { }
