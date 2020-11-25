ca210b5b59f2e09b945c9f3ed7c2eb33f33682b3
flaviomarcilio <flaviomarciliodeoliveira@gmail.com>
2020-11-25 11:24:56 -0300

    Usa conceito Redux - NGRX

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 import { HomePage } from './home.page';
+import { homeReducer } from './state/home.reducer';
+import { HomeEffects } from './state/home.effects';
 
 
 
@@ -10,7 +15,9 @@ import { HomePage } from './home.page';
   declarations: [HomePage],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forFeature('home', homeReducer),
+    EffectsModule.forFeature([HomeEffects])
   ]
 })
 export class HomeModule { }
