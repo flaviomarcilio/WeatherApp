7327b19ab8ff4a73faf7fa0b61cc30b7dff00721
flaviomarcilio <flaviomarciliodeoliveira@gmail.com>
2020-11-27 15:56:24 -0300

    Atualiza a página details

--- src/app/pages/details/containers/details/details.page.ts
+++ src/app/pages/details/containers/details/details.page.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CityDailyWeather } from 'src/app/shared/models/weather.model';
 
 import { AppState } from 'src/app/shared/state/app.reducer';
 import * as fromDetailsActions from '../../state/details.actions';
+import * as fromDetailsSelectors from '../../state/details.selectors';
 
 @Component({
   selector: 'fn-details',
@@ -13,10 +16,18 @@ import * as fromDetailsActions from '../../state/details.actions';
 
 export class DetailsPage implements OnInit {
 
+  details$: Observable<CityDailyWeather>;
+  loading$: Observable<boolean>;
+  error$: Observable<boolean>;
+
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.store.dispatch(fromDetailsActions.loadWeatherDetails());
+
+    this.details$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsEntity));
+    this.loading$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsLoading));
+    this.error$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsError));
   }
 
 }
--- src/app/pages/details/details.module.ts
+++ src/app/pages/details/details.module.ts
@@ -9,13 +9,17 @@ import { DetailsPage } from './containers/details/details.page';
 import { DetailsGuard } from './services/details.guard';
 import { detailsReducer } from './state/details.reducer';
 import { DetailsEffects } from './state/details.effects';
+import { DailyWeatherComponent } from './components/daily-weather/daily-weather.component';
+import { ComponentsModule } from 'src/app/shared/components/components.module';
 
 @NgModule({
   declarations: [
-    DetailsPage
+    DetailsPage,
+    DailyWeatherComponent
   ],
   imports: [
     CommonModule,
+    ComponentsModule,
     StoreModule.forFeature('details', detailsReducer),
     EffectsModule.forFeature([DetailsEffects]),
     RouterModule.forChild([
